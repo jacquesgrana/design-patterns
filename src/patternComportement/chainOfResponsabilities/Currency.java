7555f3681ec4a8c8c5419d5393e693611056ca1c
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-06-26 14:06:41 +0200

    7e commit - Chain Of Responsibilities

---
+++ src/patternComportement/chainOfResponsabilities/Currency.java
@@ -0,0 +1,14 @@
+package patternComportement.chainOfResponsabilities;
+
+public class Currency {
+
+	private int amount;
+
+	public Currency(int amt) {
+		this.amount = amt;
+	}
+
+	public int getAmount() {
+		return this.amount;
+	}
+}
